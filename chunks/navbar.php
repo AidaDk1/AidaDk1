fca74784130a87459fae8fd962298224472be2fd
AidaDk1 <65682383+AidaDk1@users.noreply.github.com>
2021-03-26 00:28:57 +0600

    Update navbar.php

--- chunks/navbar.php
+++ chunks/navbar.php
@@ -5,7 +5,7 @@
 		      <a href="#" class="brand-logo">KyrgyzFood</a>
 		      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 		      <ul class="right hide-on-med-and-down">
-		        <li><a href="/RestroGirls" class="hvr-grow">Home</a></li>
+		        <li><a href="about-restro-girls.php" class="hvr-grow">Home</a></li>
 		        <li><a href="about-restro-girls.php" class="hvr-grow">About Us</a></li>
 		        <li><a href="food-categories.php" class="hvr-grow">Categories</a></li>
 		        <li><a href="foods.php" class="hvr-grow">Foods</a></li>
@@ -29,8 +29,8 @@
 		</div>
 
 		  <ul class="sidenav" id="mobile-demo">
-		    <li><a href="/RestroGirls">Home</a></li>
-	        <li><a href="/RestroGirls/about-restro-girls.php">About Us</a></li>
+		    <li><a href="about-restro-girls.php">Home</a></li>
+	        <li><a href="about-restro-girls.php">About Us</a></li>
 	        <li><a href="food-categories.php">Categories</a></li>
 	        <li><a href="foods.php">Foods</a></li>
 	        <li><a href="#" onclick="toggleModal('Contact Info', 'You can contact us directly by calling to this number 05000000000. Check the bottom Footer Section of the website for more info.');">Contact</a></li>
